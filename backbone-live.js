b9166cd66641b8bdeeb332030c3744f039dc6525
Michael Abernethy <mike@cabforward.com>
2012-07-09 12:23:45 -0500

    newest version

--- backbone-live.js
+++ backbone-live.js
@@ -1,119 +1,271 @@
 /**
- *  CabForward - Austin's Leading Ruby on Rails Shop
+ *  LiveCollection looks to always stay up-to-date with the server's version of the collection using
+ *  either Pusher or polling (with more options to come). 
+ *  LiveModel does the same with a single model
  *
- *  LiveCollection uses Pusher to always maintain up-to-date collections
- *  LiveModel uses Pusher to always maintain an up-to-date model
+ *  v0.2
  *
- *  v0.1
- *  Future versions will hopefully become "live" agnostic, so you can use websockets, firebase, or long polling
+ *  Changes
+ *  0.1 - initial release
+ *  0.2 - added polling as an option, removed initialize function to keep the prototype pattern for Collections
  *
- *  mike@cabforward.com
+ *  Thanks to the following for ideas and great code ideas
+ *  http://weblog.bocoup.com/backbone-live-collections/ - good ideas on how to use polling
+ *
+ *  CabForward - Austin's Leading Rails Shop 
+ *
  *  Copyright (c) 2012 Michael Abernethy
  *  MIT Licensed (LICENSE)
  *
  */
 (function(){
 
-  'use strict';
+  	'use strict';
 
   	Backbone.LiveCollection = Backbone.Collection.extend({
 
-  		initialize : function(model, options) {
+		live : function(options) {
 
-  			// required parameters
-			this.pusher = options.pusher;
-			this.channel = options.channel;
-			this.eventType = options.eventType;
-
-			// optional parameters
-			this.channelType = "";
-			if (options.channelType === "private" || options.channelType === "presence" )
-				this.channelType = options.channelType + "-";
-
-			this.log = options.log || false;
-			if (this.log) {
-				Pusher.log = function(message) {
-					if (window.console && window.console.log) window.console.log(message);
-				};
-			}
-  		},
-
-		live : function() {
 			var _this = this;
+
+  			this.opts = options;
+
+			// default to polling if there's no arguments
+			options = options || {};
+			options.liveType = options.liveType || "poll";
+
+  			// if they've supplied a Pusher object or an existing pusherChannel, 
+  			// set it up to use Pusher
+  			if (options.pusher || options.pusherChannel) {
+  				this.liveType = "pusher";
+				this.pusher = options.pusher;
+				this.channelName = options.channelName;
+				this.eventType = options.eventType;				
+
+				// optional parameters
+				this.channelType = "";
+				if (options.channelType === "private" || options.channelType === "presence" )
+					this.channelType = options.channelType + "-";
+
+				this.log = options.log || false;
+				if (this.log) {
+					Pusher.log = function(message) {
+						if (window.console && window.console.log) window.console.log(message);
+					};
+				}
+  			} 
+  			// fail back to polling
+  			else {
+  				this.liveType = "poll";
+  				this.tries = options.tries || 5;
+  				this.interval = options.interval || 5000;
+  			}
 			
-			this.pusherChannel = this.pusher.subscribe(this.channelType + this.channel);
+			if (this.liveType === "pusher") {
 
-			this.pusherChannel.bind("add_" + this.eventType, function(model) {
-				_this.add(model);
-			});
+				if (!options.pusherChannel) {
+					this.pusherChannel = this.pusher.subscribe(this.channelType + this.channelName);
+					options.pusherChannel = this.pusherChannel; // save it in the options
+				}
+				else {
+					this.pusherChannel = options.pusherChannel;
+				}
 
-			this.pusherChannel.bind("remove_" + this.eventType, function(model) {
-				_this.remove(model);
-			});
+				this.pusherChannel.bind("add_" + this.eventType, function(model) {
+					_this.add(model);
+				});
 
-			this.pusherChannel.bind("update_" + this.eventType, function(model) {
-				_this.get(model.id).set(model);
-			});
+				this.pusherChannel.bind("remove_" + this.eventType, function(model) {
+					_this.remove(model);
+				});
 
-			return this.pusherChannel;
+				this.pusherChannel.bind("update_" + this.eventType, function(model) {
+					if (_this.get(model.id)) {
+						_this.get(model.id).set(model);
+					}
+				});
+
+				this.pusherChannel.bind("reset_" + this.eventType, function(models) {
+					_this.reset(models);
+				});
+
+				this.isLive = true;
+				return this.pusherChannel;
+			}
+			else if (this.liveType === "poll") {
+				var polledCount = 0;
+      
+			    // Cancel any potential previous stream.
+			    this.die();
+			      
+			    var update = _.bind(function() {	
+
+			    	this.isLive = true;
+			    	// Make a shallow copy of the options object.
+			        // `Backbone.collection.fetch` wraps the success function
+			        // in an outer function (line `527`), replacing options.success.
+			        // That means if we don't copy the object every poll, we'll end
+			        // up modifying the reference object and creating callback inception.
+			        //
+			        // Furthermore, since the sync success wrapper
+			        // that wraps and replaces options.success has a different arguments
+			        // order, you'll end up getting the wrong arguments.
+			        var opts = _.clone(options);
+
+			      	if (!this.tries || polledCount < this.tries) {
+				        polledCount = polledCount + 1;
+				          
+				        this.fetch(opts);
+				        this.pollTimeout = setTimeout(update, this.interval);
+			      	}
+
+			    }, this);
+
+			    update();
+			}
 		},
 
 		die : function() {
-			if (this.pusherChannel) {
-				this.pusherChannel.unbind("add_" + this.eventType);
-				this.pusherChannel.unbind("remove_" + this.eventType);
-				this.pusherChannel.unbind("update_" + this.eventType);
+
+			this.isLive = false;
+
+			if (this.liveType === "pusher") {
+				if (this.pusherChannel) {
+					this.pusherChannel.unbind("add_" + this.eventType);
+					this.pusherChannel.unbind("remove_" + this.eventType);
+					this.pusherChannel.unbind("update_" + this.eventType);
+				}
+				return this.pusherChannel;
+			}
+			else if (this.liveType === "poll") {
+				clearTimeout(this.pollTimeout);
+      			delete this.pollTimeout;
+			}
+		},
+
+		killAll : function() {
+			var c = this.die();
+			if (this.liveType === "pusher") {
 				this.pusher.unsubscribe(this.channelType + this.channel);
 			}
-			return this.pusherChannel;
+			return c;
+		},
+
+		isLive : function() {
+			return this.isLive;
 		}
 
 	});
 
 	Backbone.LiveModel = Backbone.Model.extend({
 
-		initialize : function(attributes, options) {
-
-  			// required parameters
-			this.pusher = options.pusher;
-			this.channel = options.channel;
-			this.eventType = options.eventType;
-
-			// optional parameters
-			this.channelType = "";
-			if (options.channelType === "private" || options.channelType === "presence" )
-				this.channelType = options.channelType + "-";
-			
-			this.log = options.log || false;
-			if (this.log) {
-				Pusher.log = function(message) {
-					if (window.console && window.console.log) window.console.log(message);
-				};
-			}
-		},
-
-		live : function() {
+		live : function(options) {
 			var _this = this;
 
-			this.pusherChannel = this.pusher.subscribe(this.channelType + this.channel);
+  			this.opts = options;
 
-			this.pusher.subscribe(this.channel).bind("update_" + this.eventType, function(model) {
-				if (_this.get("id") === model.id)
-				{
-					_this.set(model);
+			// default to polling if there's no arguments
+			options = options || {};
+			options.liveType = options.liveType || "poll";
+
+  			// if they've supplied a Pusher object or an existing pusherChannel, 
+  			// set it up to use Pusher
+  			if (options.pusher || options.pusherChannel) {
+  				this.liveType = "pusher";
+				this.pusher = options.pusher;
+				this.channelName = options.channelName;
+				this.eventType = options.eventType;				
+
+				// optional parameters
+				this.channelType = "";
+				if (options.channelType === "private" || options.channelType === "presence" )
+					this.channelType = options.channelType + "-";
+
+				this.log = options.log || false;
+				if (this.log) {
+					Pusher.log = function(message) {
+						if (window.console && window.console.log) window.console.log(message);
+					};
 				}
-			});
+  			} 
+  			// fail back to polling
+  			else {
+  				this.liveType = "poll";
+  				this.tries = options.tries || 5;
+  				this.interval = options.interval || 5000;
+  			}
+  			
+  			
+			if (this.liveType === "pusher") {
 
-			return this.pusherChannel;
+				if (!options.pusherChannel) {
+					this.pusherChannel = this.pusher.subscribe(this.channelType + this.channelName);
+					options.pusherChannel = this.pusherChannel; // save it in the options
+				}
+				else {
+					this.pusherChannel = options.pusherChannel;
+				}
+
+				this.pusherChannel.bind("update_" + this.eventType, function(model) {
+					if (_this.get("id") === model.id) {
+						_this.set(model);
+					}
+				});
+
+				return this.pusherChannel;
+			}
+			else if (this.liveType === "poll") {
+				var polledCount = 0;
+      
+			    // Cancel any potential previous stream.
+			    this.die();
+			      
+			    var update = _.bind(function() {	
+
+			    	this.isLive = true;
+			    	// Make a shallow copy of the options object.
+			        // `Backbone.collection.fetch` wraps the success function
+			        // in an outer function (line `527`), replacing options.success.
+			        // That means if we don't copy the object every poll, we'll end
+			        // up modifying the reference object and creating callback inception.
+			        //
+			        // Furthermore, since the sync success wrapper
+			        // that wraps and replaces options.success has a different arguments
+			        // order, you'll end up getting the wrong arguments.
+			        var opts = _.clone(options);
+
+			      	if (!this.tries || polledCount < this.tries) {
+				        polledCount = polledCount + 1;
+				          
+				        this.fetch(opts);
+				        this.pollTimeout = setTimeout(update, this.interval);
+			      	}
+
+			    }, this);
+
+			    update();
+			}
 		},
 
 		die : function() {
-			if (this.pusherChannel) {
-				this.pusherChannel.unbind("update_" + this.eventType);
-				this.pusher.unsubscribe(this.channelType + this.channel);
-			}
+			this.isLive = false;
 
-			return this.pusherChannel;
+			if (this.liveType === "pusher") {
+				if (this.pusherChannel) {
+					this.pusherChannel.unbind("update_" + this.eventType);
+					this.pusher.unsubscribe(this.channelType + this.channel);
+				}
+
+				return this.pusherChannel;
+			}
+			else if (this.liveType === "poll") {
+				clearTimeout(this.pollTimeout);
+      			delete this.pollTimeout;
+			}
+		},
+
+		isLive : function() {
+			return this.isLive;
 		}
 
 	});
